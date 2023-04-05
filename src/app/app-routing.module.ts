9a961f55a5175e5ba909a6ac12938164981be883
cinta <cintatafur@gmail.com>
2023-04-05 19:05:09 +0200

    ➕ Enrruto(Routes):app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearRegistrosComponent } from './crear-registros/crear-registros.component';
+import { ListaRegistrosComponent } from './lista-registros/lista-registros.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'registro', pathMatch: 'full'},
+  {path: 'registro', component: CrearRegistrosComponent},
+  {path: 'lista', component: ListaRegistrosComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
