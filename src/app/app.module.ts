070f1642beac7dbd6773a9032f4f02523025e929
cinta <cintatafur@gmail.com>
2023-04-06 09:47:20 +0200

    ➕ Termino(html/material):termino con html...next funcinalidades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { ListaRegistrosComponent } from './lista-registros/lista-registros.compo
 //Instalados ...npm i...
 import { NgToastModule } from 'ng-angular-popup';
 import { NgConfirmModule } from 'ng-confirm-box';
-//A/Material
+//A/Material_Componenetes
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
@@ -19,6 +19,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatSelectModule} from '@angular/material/select';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
@@ -38,6 +40,9 @@ import {MatSelectModule} from '@angular/material/select';
     MatInputModule,
     MatRadioModule,
     MatSelectModule,
+       /* date */
+    MatDatepickerModule,
+    MatNativeDateModule,
     //Instalados 
     NgToastModule,
     NgConfirmModule
