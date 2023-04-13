d35882a7b70f00981b2924215d4434b49ba22d7d
cinta <cintatafur@gmail.com>
2023-04-13 13:41:12 +0200

    💚 Add(detalles):realizo detalles de ususario

--- src/app/detalles-user/detalles-user.component.ts
+++ src/app/detalles-user/detalles-user.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { User } from './../models/user.models';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from './../services/api.service';
 
 @Component({
   selector: 'app-detalles-user',
   templateUrl: './detalles-user.component.html',
-  styleUrls: ['./detalles-user.component.scss']
+  styleUrls: ['./detalles-user.component.scss'],
 })
-export class DetallesUserComponent {
+export class DetallesUserComponent implements OnInit {
+  id!: number;
+  usuarioDetalles!: User;
 
+  constructor(private activeRoute: ActivatedRoute, private api: ApiService) {}
+
+  ngOnInit() {
+    this.activeRoute.params.subscribe((val) => {
+      this.id = val['id'];
+      this.obtenerDetallesUsuario(this.id);
+
+    });
+  }
+
+  obtenerDetallesUsuario(id: number) {
+    this.api.getRegisteredUserId(id)
+    .subscribe({
+      next: (res) => {
+        this.usuarioDetalles = res;
+        console.log(this.usuarioDetalles);
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+
+
+  
 }
