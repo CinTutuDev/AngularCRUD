011adbb9202228eb3c7a296846a7e25cc868797e
cinta <cintatafur@gmail.com>
2023-04-13 17:15:25 +0200

    💚 android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.example.app',
-  appName: 'y',
+  appName: 'Gym Registros',
   webDir: 'dist/gymangular-crud',
   bundledWebRuntime: false
 };
